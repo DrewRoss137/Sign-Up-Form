2fe0a9ff8f799015e429307553be9b8a755c51f8
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-23 20:36:48 +0000

    Improve dob input functionality

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const LastNameInput = document.getElementById("first-name-input");
 
 const userNameInput = document.getElementById("user-name-input");
 
-// const dateOfBirthInput = document.getElementById("date-of-birth-input");
+const dateOfBirthInput = document.getElementById("date-of-birth-input");
 
 const emailAddressInput = document.getElementById("email-address-input");
 
@@ -14,32 +14,48 @@ const passwordInput = document.getElementById("password-input");
 
 const confirmPasswordInput = document.getElementById("confirm-password-input");
 
-
-const dobInput = document.getElementById('date-of-birth-input');
-
-let firstClick = true;
 const dateFormat = 'DD/MM/YYYY';
+let firstClick = true;
 
-dobInput.addEventListener('focus', () => {
-  if (firstClick && (dobInput.value === '' || dobInput.value === dateFormat)) {
-    dobInput.value = dateFormat;
-    dobInput.setSelectionRange(0, 0);
+function findLastEnteredInputPosition(inputValue) {
+  let position = inputValue.length - 1;
+  while (position >= 0 && 'DMY'.includes(inputValue[position])) {
+    position--;
+  }
+  return position + 1;
+}
+
+dateOfBirthInput.addEventListener('focus', () => {
+  if (firstClick && (dateOfBirthInput.value === '' || dateOfBirthInput.value === dateFormat)) {
+    dateOfBirthInput.value = dateFormat;
+    dateOfBirthInput.setSelectionRange(0, 0);
     firstClick = false;
   } else {
-    const cursorPosition = dobInput.selectionStart;
-    dobInput.setSelectionRange(cursorPosition, cursorPosition);
+    setTimeout(() => {
+      let cursorPosition = dateOfBirthInput.value.indexOf('D');
+      if (cursorPosition === -1) {
+        cursorPosition = dateOfBirthInput.value.indexOf('M');
+      }
+      if (cursorPosition === -1) {
+        cursorPosition = dateOfBirthInput.value.indexOf('Y');
+      }
+      if (cursorPosition === -1) {
+        cursorPosition = dateOfBirthInput.value.length;
+      }
+      dateOfBirthInput.setSelectionRange(cursorPosition, cursorPosition);
+    }, 0);
   }
 });
 
-dobInput.addEventListener('blur', () => {
-  if (dobInput.value === dateFormat) {
-    dobInput.value = '';
+dateOfBirthInput.addEventListener('blur', () => {
+  if (dateOfBirthInput.value === dateFormat) {
+    dateOfBirthInput.value = '';
     firstClick = true;
   }
 });
 
-dobInput.addEventListener('input', () => {
-  const inputDigits = dobInput.value.replace(/\D/g, '');
+dateOfBirthInput.addEventListener('input', () => {
+  const inputDigits = dateOfBirthInput.value.replace(/\D/g, '');
   let formattedInput = '';
 
   for (let i = 0, j = 0; i < dateFormat.length; i++) {
@@ -50,24 +66,24 @@ dobInput.addEventListener('input', () => {
     }
   }
 
-  dobInput.value = formattedInput;
-  const nextPosition = dobInput.value.search(/D|M|Y/);
-  dobInput.setSelectionRange(nextPosition, nextPosition);
+  dateOfBirthInput.value = formattedInput;
+  const nextPosition = dateOfBirthInput.value.search(/D|M|Y/);
+  dateOfBirthInput.setSelectionRange(nextPosition, nextPosition);
 });
 
-dobInput.addEventListener('keydown', (event) => {
+dateOfBirthInput.addEventListener('keydown', (event) => {
   if (event.key === 'Backspace' || event.key === 'Delete') {
     event.preventDefault();
 
-    const currentValue = dobInput.value;
-    let position = dobInput.selectionStart;
+    const currentValue = dateOfBirthInput.value;
+    let position = dateOfBirthInput.selectionStart;
 
     if (position > 0) {
       if (currentValue[position - 1] === '/') {
         position--;
       }
-      dobInput.value = currentValue.slice(0, position - 1) + dateFormat[position - 1] + currentValue.slice(position);
-      dobInput.setSelectionRange(position - 1, position - 1);
+      dateOfBirthInput.value = currentValue.slice(0, position - 1) + dateFormat[position - 1] + currentValue.slice(position);
+      dateOfBirthInput.setSelectionRange(position - 1, position - 1);
     }
   }
 });
